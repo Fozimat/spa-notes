eacaa72ef390bf4266200b48d8a7aba8b075b134
fozimat <fozimata@gmail.com>
2023-11-12 14:50:33 +0700

    move styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import NotesApp from "./components/NotesApp";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
