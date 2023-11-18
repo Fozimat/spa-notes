e2c37623ca86cf2117e1950b9981af5538ab6d2e
fozimat <fozimata@gmail.com>
2023-11-18 10:39:50 +0700

    add theme for auth

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { login } from "../utils/network-data";
 import LoginInput from "../components/LoginInput";
+import ThemeContext from "../contexts/ThemeContext";
+import { getContainerClass } from "../utils/theme-helper";
 
 const LoginPage = ({ loginSuccess }) => {
+  const { theme } = useContext(ThemeContext);
+  const containerClass = getContainerClass(theme);
+
   const onLogin = async ({ email, password }) => {
     const { error, data } = await login({ email, password });
 
@@ -13,9 +18,9 @@ const LoginPage = ({ loginSuccess }) => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className={containerClass}>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
           <LoginInput login={onLogin} />
         </div>
       </div>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { register } from "../utils/network-data";
 import RegisterInput from "../components/RegisterInput";
+import ThemeContext from "../contexts/ThemeContext";
+import { getContainerClass } from "../utils/theme-helper";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
+
+  const { theme } = useContext(ThemeContext);
+  const containerClass = getContainerClass(theme);
 
   const onRegisterHandler = async (user) => {
     const { error } = await register(user);
@@ -16,9 +21,9 @@ const RegisterPage = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className={containerClass}>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
           <RegisterInput register={onRegisterHandler} />
         </div>
       </div>
